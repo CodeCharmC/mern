5bf77afa5e55a028f0671a785011d89a1684be6b
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-01-06 18:26:11 +0600

    Install react js and tailwind css and create the fist template

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-blue-800 font-bold text-center'>Hello, world! Love You a lot.</h1>
+    </div>
+  )
+};
+
+export default App;
