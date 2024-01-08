2e8af27c6cba440d535a67e0b24aaa31243e4796
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-01-08 20:23:33 +0600

    add redux toolkit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Header from './components/Header';
 
+
 const App = () => {
   return (
     <BrowserRouter>
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
+import { useSelector } from 'react-redux';
 
 export default function SignIn() {
    const [formData, setFormData] = useState({});
-   const [error, setError] = useState(null);
-   const [loading, setLoading] = useState(false);
-   const navigate= useNavigate();
+   const {loading, error} = useSelector((state) => state.user);
+   const navigate = useNavigate();
+   const dispatch = useDispatch();
+
   
    const handleChange = (e) => {
       setFormData({
@@ -17,27 +21,27 @@ export default function SignIn() {
    const handleSubmit = async (e) => { 
       e.preventDefault(); 
       try {
-         setLoading(true);
-         const res = await fetch('/api/auth/signin', { 
+         dispatch(signInStart());
+         const res = await fetch('/api/auth/signin', {
             method: 'POST',
             headers: {
                'Content-Type': 'application/json'
             },
             body: JSON.stringify(formData)
-         })
+         });
+         if (!res.ok) {
+            throw new Error('Failed to sign in');
+         }
          const data = await res.json();
          if (data.success === false) {
-            setLoading(false);
-            setError(data.message);            
+            dispatch(signInFailure(data.message));           
             return;
          }
-         setLoading(false);
-         setError(null);
+         dispatch(signInSuccess(data));
          navigate('/');
 
       } catch (error) {
-         setLoading(false);
-         setError(error.message);
+         dispatch(signInFailure(error.message));
       };           
    };
    return (
