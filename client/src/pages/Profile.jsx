be18ac28aadaf2f75e848dd550dee171676749b6
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-01-13 15:33:57 +0600

    add delete functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import { useRef, useState, useEffect } from 'react';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../firebase';
-import { updateUserStart, updateUserSuccess, updateUserFailure } from '../redux/user/userSlice';
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 
 
@@ -74,6 +74,23 @@ export default function Profile() {
       }
    };
 
+   const handleDeleteUser = async () => { 
+      try {
+         dispatch(deleteUserStart());
+         const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+            method: 'DELETE'
+         });
+         const data = await res.json();
+         if (data.success === false) {
+            dispatch(deleteUserFailure(data.message));
+            return;  
+         }
+         dispatch(deleteUserSuccess(data));
+      } catch (error) {
+         dispatch(deleteUserFailure(error.message));
+      }
+   }  
+
    return (
       <div className='p-3 max-w-lg mx-auto'>
          <h1 className='text-3xl font-semibold text-center my-7'>
@@ -126,9 +143,11 @@ export default function Profile() {
                {loading ? 'Loading...' : 'Update'}
             </button>
          </form>
-         <div className='flex justify-between mt-5'>
-            <span className='text-red-500'>Delete your account</span>
-            <span className='text-red-500'>Sign out</span>
+         <div
+            onClick={handleDeleteUser}
+            className='flex justify-between mt-5'>
+               <span className='text-red-500'>Delete your account</span>
+               <span className='text-red-500'>Sign out</span>
          </div>
          <p className='text-red-500 mt-5'>
             {error? error: ''}
