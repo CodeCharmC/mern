89fc979a6874494df9a89062b5910e35037c05bb
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-01-19 18:28:32 +0600

    correct the show more button functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -122,8 +122,8 @@ export default function Search() {
       const searchQuery = urlParams.toString();
       const res = await fetch(`/api/listing/get?${searchQuery}`);
       const data = await res.json();
-      if (data.length > 9) {
-         setShowMore(true);
+      if (data.length < 9) {
+         setShowMore(false);
       }
       setListings([...listings, ...data]);
    };
